73ba2c3498678db87ab1a73790797b78711aec28
kyj8765 <kyj8765@gmail.com>
2020-12-03 21:29:22 -0800

    Updates

---
+++ precache-manifest.c8e34a73f97934cdfeedd69e38f8e86d.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6f3b08bbed12190b058f2d8e88035480",
+    "url": "/covid19_data_visualization/index.html"
+  },
+  {
+    "revision": "191c497af8948ab18d2f",
+    "url": "/covid19_data_visualization/static/css/2.c37a399a.chunk.css"
+  },
+  {
+    "revision": "7d025800ab4e7a752e3f",
+    "url": "/covid19_data_visualization/static/css/main.c8d69e16.chunk.css"
+  },
+  {
+    "revision": "191c497af8948ab18d2f",
+    "url": "/covid19_data_visualization/static/js/2.964a0aa0.chunk.js"
+  },
+  {
+    "revision": "291514592b737187a68d336241fa25f7",
+    "url": "/covid19_data_visualization/static/js/2.964a0aa0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7d025800ab4e7a752e3f",
+    "url": "/covid19_data_visualization/static/js/main.f45515c7.chunk.js"
+  },
+  {
+    "revision": "5c186293a4e853c2270a",
+    "url": "/covid19_data_visualization/static/js/runtime-main.3f12f69e.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/covid19_data_visualization/static/media/logo.5d5d9eef.svg"
+  }
+]);
